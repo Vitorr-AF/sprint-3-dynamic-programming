c5cc862badcac957b417503d34b2a12543360296
Vitorr-AF <vitoraugustofranca7@gmail.com>
2025-09-05 14:40:14 -0300

    algoritimos de busca adicionados

--- base.py
+++ base.py
@@ -27,6 +27,28 @@ def ler_consultas(pilha):
         print(f"Consulta: {info['consulta']}\nConsumo: {info['consumo']}")
 
 
+def busca_linear(lista, alvo):
+    
+    for x, y in enumerate(lista):
+        if y == alvo:
+            return x
+
+    return -1
+
+
+def busca_binaria(lista, alvo):
+    f = len(lista) - 1
+    c = 0
+    while f > c:
+        i_atual = (c+f)//2
+        if lista[i_atual] == alvo:
+            return i_atual
+        elif lista[i_atual] > alvo:
+            f = i_atual-1
+        elif lista[i_atual] < alvo:
+            c = i_atual+1
+    return -1
+
 
 
 #Testes de funções
@@ -48,3 +70,13 @@ def testar():
     adicionar_consulta("Exame de urina", "potinho", consultas)
 
     ler_consultas(consultas)
+
+    print("\n---------------------\n")
+
+    insumos = ["seringa", "agulha", "gaze", "pipeta"]
+
+    print(f"Resultado busca linear: {busca_linear(insumos, 'gaze')}")
+    print(f"Resultado busca binária: {busca_binaria(insumos, 'gaze')}")
+
+
+testar()
