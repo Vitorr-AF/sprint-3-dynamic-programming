7053bb3502e7fc0d4988384d6d16af90cf5308ce
Vitorr-AF <vitoraugustofranca7@gmail.com>
2025-09-05 14:13:31 -0300

    testes adicionados à função testar()

--- base.py
+++ base.py
@@ -27,21 +27,24 @@ def ler_consultas(pilha):
         print(f"Consulta: {info['consulta']}\nConsumo: {info['consumo']}")
 
 
+
+
 #Testes de funções
-print("Teste da fila:")
-consumos = []
-adicionar_consumo("gaze", "2:00", consumos)
-adicionar_consumo("seringa", "6:00", consumos)
-adicionar_consumo("agulha", "12:00", consumos)
+def testar():
+    print("Teste da fila:")
+    consumos = []
+    adicionar_consumo("gaze", "2:00", consumos)
+    adicionar_consumo("seringa", "6:00", consumos)
+    adicionar_consumo("agulha", "12:00", consumos)
 
-ler_consumos(consumos)
+    ler_consumos(consumos)
 
-print("\n---------------------\n")
+    print("\n---------------------\n")
 
-print("Teste da pilha:")
-consultas = []
-adicionar_consulta("Exame de sangue", "seringa", consultas)
-adicionar_consulta("Ultrassom,", "gel", consultas)
-adicionar_consulta("Exame de urina", "potinho", consultas)
+    print("Teste da pilha:")
+    consultas = []
+    adicionar_consulta("Exame de sangue", "seringa", consultas)
+    adicionar_consulta("Ultrassom,", "gel", consultas)
+    adicionar_consulta("Exame de urina", "potinho", consultas)
 
-ler_consultas(consultas)
+    ler_consultas(consultas)
